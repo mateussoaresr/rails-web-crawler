a77cd47137ab31a527d0c2b1a181fef65459ca92
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2021-01-23 11:33:16 -0300

    feat: include timestamps to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User
   include ActiveModel::SecurePassword
   include Mongoid::Document
+  include Mongoid::Timestamps
+
   field :name, type: String
   field :username, type: String
   field :email, type: String
