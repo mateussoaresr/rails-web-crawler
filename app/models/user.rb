2841f12e7dd40d30f8ff1cefbe1a4b316a3e80ef
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2021-01-23 09:52:26 -0300

    chore: create model to user

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User
+  include ActiveModel::SecurePassword
+  include Mongoid::Document
+  field :name, type: String
+  field :username, type: String
+  field :email, type: String
+  field :password, type: String
+  field :password_confirmation, type: String
+  field :password_digest, type: String
+  
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username, presence: true, uniqueness: true
+  validates :password,
+            length: { minimum: 6 },
+            if: -> { new_record? || !password.nil? }
+
+  has_secure_password
+end
