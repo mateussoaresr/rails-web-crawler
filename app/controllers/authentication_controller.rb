38bdefa7bc1d285b096403774d3aafcf2ea3de80
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2021-01-23 11:32:55 -0300

    fix: correct syntax

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,7 +3,7 @@ class AuthenticationController < ApplicationController
 
   # POST /auth/login
   def login
-    @user = User.find_by_email(params[:email])
+    @user = User.find_by(email: params[:email])
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
