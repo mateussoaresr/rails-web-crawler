69e9cbffd90096a0c0e4af49d27d8c2ad4f1564e
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2021-01-23 11:32:36 -0300

    fix: replace active record error by mongo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
 
   def find_user
     @user = User.find_by_username!(params[:_username])
-    rescue ActiveRecord::RecordNotFound
+    rescue Mongo::Error
       render json: { errors: 'User not found' }, status: :not_found
   end
 
