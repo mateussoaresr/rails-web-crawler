83d5874c06a9d149cbf8693466184607aff72e6e
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2021-01-23 11:31:30 -0300

    feat: adapt responses to jbuilder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,17 @@ class UsersController < ApplicationController
   # GET /users
   def index
     @users = User.all
-    render json: @users, status: :ok
   end
 
   # GET /users/{username}
   def show
-    render json: @user, status: :ok
   end
 
   # POST /users
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created
+      render json: {notice: "Usuário criado com sucesso."}, status: :created
     else
       render json: { errors: @user.errors.full_messages },
               status: :unprocessable_entity
