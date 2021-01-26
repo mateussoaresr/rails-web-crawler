29320942f85335aca26df0eb9041085657577936
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2021-01-26 18:00:38 -0300

    chore: create quote model

---
+++ app/models/quote.rb
@@ -0,0 +1,18 @@
+class Quote
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :title, type: String
+  field :author, type: String
+  field :about, type: String
+
+  validates :title, presence: true, uniqueness: true
+  validates :author, presence: true, uniqueness: true
+
+  has_and_belongs_to_many :tags
+  accepts_nested_attributes_for :tags
+  
+  def tag_names
+    tags.pluck(:name)
+  end
+end
