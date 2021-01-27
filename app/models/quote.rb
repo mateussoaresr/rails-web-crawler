ca5345b84726bf317490518dcf7aa61d72015b12
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2021-01-27 20:12:18 -0300

    chore: add belongs_to, accept nested attributes and delegates to author

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,14 +3,16 @@ class Quote
   include Mongoid::Timestamps
 
   field :title, type: String
-  field :author, type: String
-  field :about, type: String
 
   validates :title, presence: true, uniqueness: true
-  validates :author, presence: true, uniqueness: true
 
+  belongs_to :author
   has_and_belongs_to_many :tags
   accepts_nested_attributes_for :tags
+  accepts_nested_attributes_for :author
+
+  delegate :name, to: :author, prefix: true
+  delegate :author_about, to: :author
 
   def tag_names
     tags.pluck(:name)
