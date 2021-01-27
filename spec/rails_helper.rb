1d27c7638189d410bb3e26afc6b5aba6af809f1f
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2021-01-27 20:15:41 -0300

    chore: run rspec:install and add mongoid-rspec configs

---
+++ spec/rails_helper.rb
@@ -0,0 +1,16 @@
+require "spec_helper"
+
+ENV["RAILS_ENV"] ||= "test"
+
+require File.expand_path("../config/environment", __dir__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+require "rspec/rails"
+require "mongoid-rspec"
+
+RSpec.configure do |config|
+  config.include Mongoid::Matchers, type: :model
+  config.use_active_record = false
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
